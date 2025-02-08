2487ffdbe8c2b99add2111fb00e56036bb9f33cf
DeM4rcio <henriquemarcio170@gmail.com>
2025-02-08 17:28:07 -0300

    criação da função iterativa problema 62

--- 62. Unique Paths/main.py
+++ 62. Unique Paths/main.py
@@ -16,4 +16,15 @@ class Solution:
             return dp(i + 1, j) + dp(i, j + 1)
         
         return dp(0, 0)  
-
+    
+    # Função iterativa
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        dp = [[1] * n for _ in range(m)]
+        
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        
+        return dp[m - 1][n - 1] 
