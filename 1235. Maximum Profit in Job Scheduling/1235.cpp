e30d77d656066960899c8d12fc1c80659ded933b
Cássio Reis <csreis04@gmail.com>
2025-02-08 16:15:19 -0300

    Ordena o vetor pelo endTime

--- 1235. Maximum Profit in Job Scheduling/1235.cpp
+++ 1235. Maximum Profit in Job Scheduling/1235.cpp
@@ -1,9 +1,30 @@
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+struct Job
+{
+    int startTime;
+    int endTime;
+    int profit;
+};
 
 class Solution {
 public:
+
+    bool compareByEndTime(Job &a, Job &b) {
+        return a.endTime < b.endTime;
+    }
+
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = startTime.size();
+
+        std::vector<Job> jobs(n);
+
+        for (int i = 0; i < n; i++)
+            jobs.push_back({startTime[i], endTime[i], profit[i]});
+
+        std::sort(jobs.begin(), jobs.end(), compareByEndTime);
         
     }
 };
