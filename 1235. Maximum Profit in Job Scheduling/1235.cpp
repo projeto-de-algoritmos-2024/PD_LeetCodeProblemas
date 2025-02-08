f0eb90dab4550f8660e7643ef9eb3fb8dff9a598
Cássio Reis <csreis04@gmail.com>
2025-02-08 16:49:01 -0300

    Adiciona função para computra p(j)

--- 1235. Maximum Profit in Job Scheduling/1235.cpp
+++ 1235. Maximum Profit in Job Scheduling/1235.cpp
@@ -12,8 +12,21 @@ struct Job
 class Solution {
 public:
 
-    bool compareByEndTime(Job &a, Job &b) {
+    static bool compareByEndTime(Job &a, Job &b) {
         return a.endTime < b.endTime;
+    }
+
+    void computeP(vector<int> &p, vector<Job> &jobs, int n) {
+        for (int i = 0; i < n; i++) {
+            p[i] = 0;
+
+            for (int j = i - 1; j >= 0; j--) {
+                if (jobs[i].startTime >= jobs[j].endTime) {
+                    p[i] = j;
+                    break;
+                }
+            }
+        }   
     }
 
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
@@ -22,9 +35,12 @@ public:
         std::vector<Job> jobs(n);
 
         for (int i = 0; i < n; i++)
-            jobs.push_back({startTime[i], endTime[i], profit[i]});
+            jobs[i] = {startTime[i], endTime[i], profit[i]};
 
         std::sort(jobs.begin(), jobs.end(), compareByEndTime);
         
+        vector<int> p(n);
+
+        computeP(p, jobs, n);
     }
 };
